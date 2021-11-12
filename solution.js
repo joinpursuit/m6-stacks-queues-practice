5fe52ca92a836080953bc8a1db66325127c8c4bc
Yesenia L <ylopezcr123@gmail.com>
2021-11-12 18:52:36 -0500

    missing 6

--- solution.js
+++ solution.js
@@ -1,6 +1,108 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor (data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.max = null;
+    this.size = 0;
+  }
+  enqueue(data) {
+    let newItem = new Node(data);
+    if (!this.first) {
+      this.first = newItem;
+      this.last = newItem;
+    } else {
+      this.last.next = newItem;
+      this.last = newItem;
+    }
+    return ++this.size;
+  }
+  isEmpty() {
+    return this.first === null;
+  
+  }
+  dequeue() {
+    if (this.first == null) {
+      throw new Error("The queue is empty");
+    }
+    const item = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return item.data;
+  }
+  peek() {
+    if (this.first == null) {
+      throw new Error("The queue is empty");
+    }
+    return this.first;
+  }
+  last() {
+
+  }
+  size() {
+
+  }
+  max() {
+
+  }
+}
+
+class Stack {
+  constructor(top = null) {
+    this.top =top;
+  }
+  isEmpty () {
+    return this.top === null;
+  }
+  push(data) {
+    const newItem = new Node(data);
+    newItem.next = this.top;
+    this.top = newItem;
+  }
+  peek() {
+    if(this.top == null) {
+      throw new Error('This stack is empty')
+    }
+    return this.top;
+  }
+  pop () {
+    if (this.top == null) {
+      throw new Error("The stack is empty");
+    }
+    let item = this.top;
+    if (item) {
+      let newItem = item.next;
+      this.top = newItem;
+      return item;
+    }
+  }
+  size() {
+  
+  }
+  findMin () {
+
+  }
+  sort () {
+
+  }
+
+  
+   
+  
+  
+}
 module.exports = {
   Node,
   Queue,
