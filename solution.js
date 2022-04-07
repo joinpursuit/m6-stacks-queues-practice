dd07d1f07796a6ff1c16f2ca7dbffe0d67f37e17
RohanPursuit <rohansterling@pursuit.org>
2022-04-07 13:13:38 -0400

    add constructor for Node class with data and next property

--- solution.js
+++ solution.js
@@ -2,7 +2,10 @@ const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
 class Node{
-
+  constructor(data, next = null){
+    this.data = data
+    this.next = next
+  }
 }
 
 class Queue{
@@ -10,7 +13,7 @@ class Queue{
 }
 
 class Stack{
-  
+
 }
 
 module.exports = {
