b6317447340a5d5ecfba6d12d5c0d43f28206990
RohanPursuit <rohansterling@pursuit.org>
2022-04-07 13:11:34 -0400

    npm install, add Node, Queue and Stack classes

--- solution.js
+++ solution.js
@@ -1,6 +1,18 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node{
+
+}
+
+class Queue{
+
+}
+
+class Stack{
+  
+}
+
 module.exports = {
   Node,
   Queue,
