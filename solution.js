b6aa84db4541a16230b79e9554223b775335dddc
Melissa Lopez <melissalopez@pursuit.org>
2022-04-08 17:45:13 -0400

    completed with Steph

--- solution.js
+++ solution.js
@@ -30,7 +30,6 @@ class Stack {
 
   size(){
     let valueOfSize = 0;
-    // console.log(inspect(this.top, {color: true, depth:12}))
     let currentNode = this.top //current node is at the top of the stack
     while(currentNode !== null){ //while current node isn't empty
       valueOfSize++ //incrementing by one
@@ -50,9 +49,8 @@ class Stack {
 
 
   findMin() {
-    //> output: number
     let minimum = this.top.data;  //0
-    console.log(minimum)
+    // console.log(minimum)
 
     let currentItem = this.top //> start
     while(currentItem !== null) { //current item is not empty,
@@ -69,10 +67,10 @@ class Stack {
   sort() {
     //> start, ..., next
     let minimumValue = this.top; 
-    console.log(minimumValue)
+    // console.log(minimumValue)
 
     let currentItem = this.top //updates if it's the minimum value
-    console.log(currentItem)
+    // console.log(currentItem)
     while(currentItem !== null) {
       //> compare a `number` to a `number`
       if (currentItem.data < minimumValue.data) {
@@ -81,7 +79,7 @@ class Stack {
       }
       //> next item
       currentItem = currentItem.next 
-      console.log(currentItem)
+      // console.log(currentItem)
     }
     minimumValue.next = this.top //min value goes to the top of stack
     this.top = minimumValue
@@ -98,16 +96,64 @@ class Queue {
     this.size = 0;
     this.maxValue = 0;
   }
+
+  enqueue(data){//creating a new queue
+    let newNode = new Node(data);
+    if(!this.first){ //if not empty,
+      this.first = newNode; // first & last becomes new node
+      this.last = newNode;
+    } else {
+      this.last.next = newNode; //else, the next item becomes new Node
+      this.last = newNode; //reassign new last position in the queue
+    }
+    return this.size++; //increase size by 1
+  }
+
+  peek(){
+    return this.first;
+    // console.log(this.first)
+  }
+
+  getLast(){
+    return this.last;
+  }
+
+  isEmpty(){
+    return this.first === null;
+  }
+
+  
+
+  findMax() {
+    let maximum = this.first;
+    let current = this.first
+    while (current) {
+      if(maximum.data < current.data) { //comparing the data values
+        maximum = current;
+      }
+     current = current.next;
+    } 
+    return maximum.data;
+  }
+
+  count(){
+    return this.size; //size is already set equal to 0
+  }
+
+  dequeue() {
+    if (!this.first) { //if empty, throw error
+        throw new Error("The queue is empty"); //error handling
+    }
+    const item = this.first;
+    if (this.first === this.last) { //if first position is also the last position, return null
+        this.last = null;
+    }
+    this.first = this.first.next; //new first person in stack
+    this.size--; //decreasing size
+    return item.data;
 }
 
-
-
-
-
-
-
-
-
+}
 
 module.exports = {
   Node,
