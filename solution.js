b286cb2366afa089c8d87b97bed56d3a201a0ec0
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-04-11 17:27:33 -0400

    modified a file

--- solution.js
+++ solution.js
@@ -1,19 +1,19 @@
-// const { nums, words } = require("./data/data.js");
-// const { inspect } = require("util");
+const { nums, words } = require("./data/data.js");
+const { inspect } = require("util");
 
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
 
-const words = [
-  "the",
-  "quick",
-  "brown",
-  "fox",
-  "jumps",
-  "over",
-  "the",
-  "lazy",
-  "dog",
-];
+// const words = [
+//   "the",
+//   "quick",
+//   "brown",
+//   "fox",
+//   "jumps",
+//   "over",
+//   "the",
+//   "lazy",
+//   "dog",
+// ];
 
 class Node {
   constructor(data) {
@@ -42,8 +42,20 @@ class Stack {
       return item;
     }
   }
+  size() {
+    
+  }
   isEmpty() {
     return this.top === null;
+  }
+  peek() {
+    if (this.top === null) {
+      throw new Error("The stack is empty");
+    }
+    return this.top;
+  }
+  sort() {
+    
   }
 }
 
@@ -52,6 +64,41 @@ class Queue {
     this.first = null;
     this.last = null;
     this.size = 0;
+  }
+  enqueue(data) {
+    let newItem = new Node(data);
+    if (!this.first) {
+      this.first = newItem;
+      this.last = newItem;
+    } else {
+      this.last.next = newItem;
+      this.last = newItem;
+    }
+    return ++this.size
+  }
+  dequeue() {
+    if (this.first === null) {
+      throw new Error("The queue is empty");
+    }
+    const item = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return item.data;
+  }
+  peek() {
+    if (this.first === null) {
+      throw new Error("the queue is empty");
+    }
+    return this.first;
+  }
+  count() {
+    
+  }
+  isEmpty() {
+    return this.first === null;
   }
 }
 
