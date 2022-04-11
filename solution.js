987ddd99e742821af8611aee10bd3209401f040e
Milton Clark <kingcoder247@gmail.com>
2022-04-10 20:27:08 -0400

    Create Node and Stack class, Stack Methods: push, size, pop, isEmpty, findMin, peek, and sort with no functionality

--- solution.js
+++ solution.js
@@ -1,8 +1,53 @@
+// Dependencies
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node{
+  constructor(data){
+    this.data = data;
+    this.next = null;
+  };
+};
+
+class Stack{
+  constructor(top = null){
+    this.top = top;
+  };
+  /////////////////// Stack Methods////////////////
+  push(){
+
+  };
+
+  size(){
+
+  };
+
+  pop(){
+
+  };
+
+  isEmpty(){
+
+  };
+
+  findMin(){
+
+  };
+
+  peek(){
+
+  };
+
+  sort(){
+
+  };
+
+};
+
+
+
 module.exports = {
   Node,
-  Queue,
+  // Queue,
   Stack,
 };
