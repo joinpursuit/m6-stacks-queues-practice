4eafe3d61f1f93c6da2918149252687226726077
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-10-18 20:56:15 -0400

    adds stack class and five methods

--- solution.js
+++ solution.js
@@ -1,6 +1,76 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node{
+  constructor(data){
+    this.data = data;
+    this.next = null;
+  }
+}
+//first one in last one out
+class Stack{
+  constructor(top = null){
+    this.top = top;
+  }
+
+  push(data){
+    //creating our new node with the data
+    const newItem = new Node(data);
+    //we're setting the newItems NEXT box to the current top
+    newItem.next = this.top;
+    //we're making the newItem the top
+    this.top = newItem;
+  }
+
+  size(){
+    let count = 0;
+    let node = this.top;
+    while(node){
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  pop(){
+    const item = this.top;
+
+    if(item){
+      let newItem = item.next;
+      this.top = newItem;
+      return item;
+    }
+  }
+
+  isEmpty(){
+    return this.top === null;
+  }
+
+  findMin(){
+
+
+  }
+
+  peek(){
+    return this.top;
+  }
+
+  sort(){
+
+  }
+
+}
+
+class Queue{
+  constructor(value) {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+    this.max = value;
+  }
+
+}
+
 module.exports = {
   Node,
   Queue,
