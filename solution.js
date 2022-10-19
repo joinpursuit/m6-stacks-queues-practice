c7874e45fee60547e1a772fc28c0fc810e0ffbad
Derek <plutus.derekpeterson@gmail.com>
2022-10-18 22:04:18 -0400

    Completed Queue

--- solution.js
+++ solution.js
@@ -15,7 +15,7 @@ class Stack {
 
   findMin() {
     let arr = this.toArray();
-    return this.top !== null ? Math.min(...arr) : 'The stack is empty';
+    return this.top !== null ? Math.min(...arr) : 'Error! The stack is empty!';
   }
 
   isEmpty() {
@@ -23,12 +23,12 @@ class Stack {
   }
 
   peek() {
-    return this.top !== null ? this.top : 'Error! The stack is empty';
+    return this.top !== null ? this.top : 'Error! The stack is empty!';
   }
 
   pop() {
     if (this.top === null) {
-      throw new Error('The stack is empty');
+      throw new Error('Error! The stack is empty!');
     }
     let item = this.top;
     if (item) {
@@ -82,6 +82,67 @@ class Queue {
     this.max = null;
     this.last = null;
     this.first = null;
+  }
+
+  count() {
+    return this.size;
+  }
+
+  dequeue() {
+    if (this.first === null) {
+      throw new Error('Error! The queue is empty!');
+    }
+    let item = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return item.data;
+  }
+
+  enqueue(data) {
+    let newItem = new Node(data);
+    if (!this.first) {
+      this.first = newItem;
+      this.last = newItem;
+    } else {
+      this.last.next = newItem;
+      this.last = newItem;
+    }
+    return this.size++;
+  }
+
+  findMax() {
+    let arr = this.toArray();
+    return this.first !== null
+      ? Math.max(...arr)
+      : 'Error! The queue is empty!';
+  }
+
+  isEmpty() {
+    return this.first === null;
+  }
+
+  getLast() {
+    return this.last;
+  }
+
+  peek() {
+    if (this.first === null) {
+      throw new Error('Error! The queue is empty!');
+    }
+    return this.first;
+  }
+
+  toArray() {
+    let array = [];
+    let currentItem = this.first;
+    while (currentItem) {
+      array.push(currentItem.data);
+      currentItem = currentItem.next;
+    }
+    return array;
   }
 }
 
