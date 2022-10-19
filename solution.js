9966076af7fc8720199881d9acc705197fbef897
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-10-18 21:45:54 -0400

    adds queue and five methods

--- solution.js
+++ solution.js
@@ -1,19 +1,19 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
-class Node{
-  constructor(data){
+class Node {
+  constructor(data) {
     this.data = data;
     this.next = null;
   }
 }
 //first one in last one out
-class Stack{
-  constructor(top = null){
+class Stack {
+  constructor(top = null) {
     this.top = top;
   }
 
-  push(data){
+  push(data) {
     //creating our new node with the data
     const newItem = new Node(data);
     //we're setting the newItems NEXT box to the current top
@@ -22,54 +22,111 @@ class Stack{
     this.top = newItem;
   }
 
-  size(){
+  size() {
     let count = 0;
     let node = this.top;
-    while(node){
+    while (node) {
       count++;
       node = node.next;
     }
     return count;
   }
 
-  pop(){
+  pop() {
     const item = this.top;
 
-    if(item){
+    if (item) {
       let newItem = item.next;
       this.top = newItem;
       return item;
     }
   }
 
-  isEmpty(){
+  isEmpty() {
     return this.top === null;
   }
 
-  findMin(){
+  findMin() {
+    let node = this.top;
+    let min = 0;
 
+    while (node) {
+      if (node < node.next) {
+        min = node.data;
+      }
+      node = node.next;
+    }
 
+    return min;
   }
 
-  peek(){
+  peek() {
     return this.top;
   }
 
-  sort(){
+  sort() {
 
+    
   }
-
 }
 
-class Queue{
-  constructor(value) {
+class Queue {
+  constructor() {
     this.first = null;
     this.last = null;
     this.size = 0;
-    this.max = value;
+    this.max = 0;
   }
 
+  count() {
+    let count = 0;
+    let node = this.first;
+  }
+
+  dequeue() {
+    let item = this.first; 
+    if (this.first === this.last){
+      this.last = null; 
+    }
+
+    this.first = this.first.next;
+    this.size --;
+  
+    return item.data;
+  }
+
+  enqueue(data) {
+    let newItem = new Node(data);
+
+    if (!this.first) {
+      this.first = newItem;
+      this.last = newItem;
+    } else {
+      this.last.next = newItem;
+      this.last = newItem;
+    }
+    return ++this.size;
+  }
+
+  findMax() {
+   
+  }
+
+  getLast() {
+    return this.last;
+  }
+
+  isEmpty() {
+    return this.first === null;
+  }
+
+  peek() {
+    return this.first;
+  }
 }
+
+// console.log(new Stack(nums));
+// console.log(new Queue(words));
 
 module.exports = {
   Node,
