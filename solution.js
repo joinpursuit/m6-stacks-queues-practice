418ca927fa68c0d6a02da2757a19cf32f1871c06
TaryneLeach <taryneleach@gmail.com>
2022-10-18 22:34:30 -0400

    node class

--- solution.js
+++ solution.js
@@ -1,6 +1,14 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data
+    this.next = null
+  }
+  
+}
+
 module.exports = {
   Node,
   Queue,
