b35603d8f25025c59ca344ff2383ad28f5251609
Andres Varon <anvaron@gmail.com>
2022-10-31 10:17:44 -0400

    Initial commit

--- solution.js
+++ solution.js
@@ -1,6 +1,51 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+
+  }
+}
+
+class Stack {
+  constructor(top = null) {
+    this.top = top;
+  }
+  isEmpty() {
+    return this.top === null;
+  }
+  push(data) {
+    const newItem = new Node(data);
+    newItem.next = this.top;
+    this.top = newItem;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(data) {
+    let newItem = new Node(data);
+    if (!this.first) {
+      this.first = newItem;
+      this.last = newItem;
+    } else {
+      this.last.next = newItem;
+      this.last = newItem;
+    }
+    return ++this.size;
+  }
+  isEmpty() {
+    return this.first === null;
+  }
+}
+
 module.exports = {
   Node,
   Queue,
